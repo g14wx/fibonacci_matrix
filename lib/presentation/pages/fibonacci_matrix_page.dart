decfb14644361db6478fddd6f5895f899a77e81c
g14wx <carlos3.hernandez@catolica.edu.sv>
2022-10-22 18:45:41 -0600

    feature(Package configuration: GetIt): Add service locator getIt Setup

--- lib/presentation/pages/fibonacci_matrix_page.dart
+++ lib/presentation/pages/fibonacci_matrix_page.dart
@@ -6,6 +6,11 @@ class FibonacciMatrixPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Matriz de Fibonacci', style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Center(
         child: Text('Fibonacci Main Page'),
       ),
