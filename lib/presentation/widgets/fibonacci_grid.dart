ed8333d64a3982871ff024e56a528c6d6d19ed79
g14wx <carlos3.hernandez@catolica.edu.sv>
2022-10-23 20:17:05 -0600

    fix(Fibonacci Controller): Add critical fix for bug
    Add a critical bug fix for logic.

--- lib/presentation/widgets/fibonacci_grid.dart
+++ lib/presentation/widgets/fibonacci_grid.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class FibonacciGrid extends StatelessWidget {
   final List<List<int>> _listFibonacci;
-  const FibonacciGrid({Key? key, required List<List<int>> listFibonacci})
+  FibonacciGrid({Key? key, required List<List<int>> listFibonacci})
       : _listFibonacci = listFibonacci,
         super(key: key);
+  final formatValues = NumberFormat.decimalPattern('en_us');
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,15 @@ class FibonacciGrid extends StatelessWidget {
                   child: Row(
                     mainAxisSize: MainAxisSize.max,
                     children: row
-                        .map((fibonacciNumber) => Flexible(child: Center(child: Text(fibonacciNumber.toString(), style: TextStyle(fontWeight: FontWeight.bold),))))
+                        .map((fibonacciNumber) => Flexible(
+                            child: Center(
+                                child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                                    child: Text(
+                                      formatValues.format(fibonacciNumber),
+                                      style: const TextStyle(fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.center,
+                                    )))))
                         .toList(),
                   ),
                 ))
